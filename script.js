153e2fcfb87e2959df068483a24b80efd403ce5a
Marc Edison <108336842+sdevmarc@users.noreply.github.com>
2023-04-25 02:07:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
+
+
+
+// Scroll Reveal
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+delay: 200
+})
+
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+
+const typed = new Typed('.multiple-text', {
+    strings: [' Basic Video Editing Services', ' Advanced Video Editing Services', 'Video Marketing Services'],
+    typeSpeed: 30,
+    backSpeed: 30,
+    backDelay: 1000,
+    loop: true
+});
